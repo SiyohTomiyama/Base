6fb7e1a7ef5afc89d54e9d5056bbe46ad5445874
SiyohTomiyama <33412935+SiyohTomiyama@users.noreply.github.com>
2025-07-15 21:35:06 +0900

    nega

--- hankoya/hankoya_sample_save.jsx
+++ hankoya/hankoya_sample_save.jsx
@@ -1,4 +1,5 @@
 ﻿var PRINT_LAYER = app.activeDocument.layers["印面"];
+var DEST_FOLDER = "~/Downloads/hankoyaData/";
 
 function resaveFile(doc, savePath) {
     var option = new IllustratorSaveOptions();
@@ -19,21 +20,7 @@ function saveTiff(doc) {
         }
     }
     var cutLine = cutLayer.pathItems[0];
-    var strokeColor = new RGBColor();
-    strokeColor.red = 0;
-    strokeColor.green = 255;
-    strokeColor.blue = 255;
-
-    cutLine.strokeColor = strokeColor;
-
     var tFrame = PRINT_LAYER.textFrames[0];
-    // 文字の塗り色を設定
-    var fillColor = new RGBColor();
-    fillColor.red = 255;
-    fillColor.green = 255;
-    fillColor.blue = 255;
-
-    tFrame.textRange.fillColor = fillColor;
     tFrame.move(cutLayer, ElementPlacement.PLACEATBEGINNING);
 
     for (var i = doc.layers.length - 1; i >= 0; i--) {
@@ -45,29 +32,37 @@ function saveTiff(doc) {
         }
     }
 
-    var tempBounds = cutLine.controlBounds;
-    var minX = tempBounds[0];
-    var maxY = tempBounds[1];
-    var maxX = tempBounds[2];
-    var minY = tempBounds[3];
+    doc.artboards[0].artboardRect = cutLine.visibleBounds;
 
-    var margin = 0;
-    doc.artboards[0].artboardRect = [minX - margin, maxY + margin, maxX + margin, minY - margin];
+    var tempName = doc.name.split('.')[0] + "_nega.ai";
+    var tempFilePath = new File(DEST_FOLDER + "/" + tempName);
+    var option = new IllustratorSaveOptions();
+    option.pdfCompatible = true;
+    doc.saveAs(File(tempFilePath), option);
 
-    // アートボードの座標を取得 (left, top, right, bottom)
-    var artboardRect = doc.artboards[0].artboardRect;
-    var artboardLeft = artboardRect[0];
-    var artboardTop = artboardRect[1];
-    var artboardRight = artboardRect[2];
-    var artboardBottom = artboardRect[3];
+    tFrame.selected = true;
+    app.executeMenuCommand("outline");
 
-    // アートボードの幅と高さを計算
-    var artboardWidth = artboardRight - artboardLeft;
-    var artboardHeight = artboardTop - artboardBottom;
+    var option = new PDFSaveOptions(); //X-4
+    option.preserveEditability = false;
+    var fileName = doc.name.split('_nega.')[0] + ".pdf";
+    var destFile = new File(DEST_FOLDER + "/" + fileName);
 
-    // 新しい長方形を作成
-    // doc.pathItems.rectangle(top, left, width, height) の順で指定
-    var rect = doc.pathItems.rectangle(artboardTop, artboardLeft, artboardWidth, artboardHeight);
+    doc.saveAs(destFile, option);
+    doc.close(SaveOptions.DONOTSAVECHANGES);
+
+    app.open(File(tempFilePath));
+    doc = app.activeDocument;
+
+    var cutLine = doc.layers[0].pathItems[0];
+    var tFrame = doc.layers[0].textFrames[0];
+
+    var strokeColor = new RGBColor();
+    strokeColor.red = 0;
+    strokeColor.green = 255;
+    strokeColor.blue = 255;
+
+    cutLine.strokeColor = strokeColor;
 
     // 塗りの色をRGB(0, 0, 0) (黒) に設定
     var fillColor = new RGBColor();
@@ -75,29 +70,39 @@ function saveTiff(doc) {
     fillColor.green = 0;
     fillColor.blue = 0;
 
-    rect.fillColor = fillColor; // 塗り色を設定
-    rect.filled = true;          // 塗りを有効にする
-    rect.stroked = false;        // 線を無効にする (塗りだけにするため)
+    cutLine.fillColor = fillColor;
 
-    // 作成した長方形を最背面へ移動
-    rect.zOrder(ZOrderMethod.SENDTOBACK);
+    // 文字の塗り色を設定
+    var fillColor = new RGBColor();
+    fillColor.red = 255;
+    fillColor.green = 255;
+    fillColor.blue = 255;
+
+    tFrame.textRange.fillColor = fillColor;
+
+    doc.artboards[0].artboardRect = cutLine.controlBounds;
+
+    tFrame.selected = true;
+    app.executeMenuCommand("outline");
 
     var option = new PDFSaveOptions(); //X-4
     option.preserveEditability = false;
 
     // ファイル名を生成
     // 現在のドキュメント名を使用し、拡張子を.tiffにする
-    var destFolder = "~/Downloads/hankoyaData/";
     var fileName = doc.name.split('.')[0] + ".pdf";
-    var destFile = new File(destFolder + "/" + fileName);
+    var destFile = new File(DEST_FOLDER + "/" + fileName);
+
     doc.saveAs(destFile, option);
+    doc.close(SaveOptions.DONOTSAVECHANGES);
+
+    File(tempFilePath).remove();
 }
 
 function main() {
     var doc = app.activeDocument;
     resaveFile(doc, doc.fullName)
     saveTiff(doc);
-    doc.close(SaveOptions.DONOTSAVECHANGES);
     alert("IllustratorとPDFファイルを保存しました");
 }
 
